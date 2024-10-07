92386c96b407b9a8f27fc09b1947684b22f43245
CarolinaOM <caro13081991@gmail.com>
2024-10-07 15:41:25 +0100

    hook: useState

--- my-app/src/components/Welcomen.js
+++ my-app/src/components/Welcomen.js
@@ -1,11 +1,17 @@
-export default function Welcomen(props){
-    console.log(props);
-    const {message, name} = props;
+import { useState } from 'react';
 
-    return(
+export default function Welcomen(props) {
+    // Cambiar la desestructuración de un objeto a una matriz
+    const [counter, setCounter] = useState(0);
+    const { message, name } = props;
+
+    return (
         <div>
             <p>Hola {name}</p>
             <p>{message}</p>
+            <h2>Contador de React con Hook</h2>
+            <h4>El número del contador es {counter}</h4>
+            <button type="submit" onClick={() => setCounter(counter + 1)}>Sumar Contador</button>
         </div>
     );
 }
