aa2a3028032d60e1b850cf6a4ec51d806d534c58
CarolinaOM <caro13081991@gmail.com>
2024-10-08 17:04:15 +0100

    hook: useEffect

--- my-app/src/components/Welcomen.js
+++ my-app/src/components/Welcomen.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 
 export default function Welcomen(props) {
-    // Cambiar la desestructuración de un objeto a una matriz
     const [counter, setCounter] = useState(0);
+    const [semaforo, setSemafoto] = useState(false);
     const { message, name } = props;
 
+    const contar = () =>{
+        setCounter(counter+1);
+        setSemafoto(!semaforo)
+    }
+    console.log(semaforo);
     return (
         <div>
             <p>Hola {name}</p>
             <p>{message}</p>
             <h2>Contador de React con Hook</h2>
             <h4>El número del contador es {counter}</h4>
-            <button type="submit" onClick={() => setCounter(counter + 1)}>Sumar Contador</button>
+            <p>El semáforo está en color {semaforo ? 'red' : 'green'}</p>
+            <button type='submit' onClick={contar}>Sumar Contador</button>
         </div>
     );
 }
